996cb3c3a50d98a749ddac688fac0754ef89fd14
gerretkubota <gerretkubota@gmail.com>
2019-05-07 03:41:13 -0700

    Cleaning up and adding comments

--- src/client/containers/AboutUsContainer.jsx
+++ src/client/containers/AboutUsContainer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import axios from 'axios';
-// import { TransitionGroup } from 'react-transition-group';
 import employeeData from '../data/data.json';
 import { rearrangePane, checkActive } from '../utils/helperFunctions.js';
 
--- src/client/containers/PaneContainer.jsx
+++ src/client/containers/PaneContainer.jsx
@@ -3,6 +3,17 @@ import PropTypes from 'prop-types';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Pane from '../components/Pane.jsx';
 
+/**
+ *
+ * @component {TransitionGroup}
+ * Manages a list of transition components
+ * Manages the mounting and unmounting of components over time
+ * @component {CSSTransition}
+ * Helps add the animations/css when the component enters/exits
+ * It will look through the CSS file and find the corresponding "status" of the component
+ * If the component is in the entering process it will find move-enter
+ * and when it's active it'll look for move-enter-active, vice versa for exit
+ */
 const PaneContainer = ({ paneQueue }) => (
   <TransitionGroup className="pane-container">
     {paneQueue.map(emp => (
